223c0170a5ce352c285b1d62cf4244005fcceb29
KennyMcDowell <kenny_mcdowell@alumni.brown.edu>
2020-07-08 12:41:02 -0400

    Assignment 2 for Cousera course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a function that creates a matrix and stores the inverse of the matrix into the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## If inverse matrix of X has been cached, return it
+## If not, calculate the inverse matrix of X
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mtrx <- x$get()
+  inv <- solve(mtrx, ...)
+  x$setinverse(inv)
+  inv
 }
+
+
